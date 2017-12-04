b28178529bce4f97b031b93e4b4e8ae05fac2663
DIDIx13 <DIDI.ameli@gmail.com>
2017-12-04 17:08:28 +0100

    Add files via upload

---
+++ Concert/Concert/Concert/Concert.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concert
+{
+    class Concert
+    {
+        private string salle;
+        private string lieu;
+        private DateTime date;
+        private double cachet;
+        private int NbSpectateurs;
+        private int note;
+        private string commentaires;
+        private string avantagesDivers;
+
+        public Concert()
+        {
+
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
