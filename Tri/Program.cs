91864bfa6fdd532e785fe6ad6c982de629560b2a
DIDIx13 <DIDI.ameli@gmail.com>
2018-01-11 14:04:05 +0100

    Create Bubble Sort

---
+++ Tri/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+
+namespace Tri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Tab = Sorting.FillTab();
+            Sorting.PrintIndexAndValues(Tab);
+            Console.Write("\n\n\n");
+            Stopwatch Timer = new Stopwatch();
+            Timer.Start();
+            Sorting.SortingTab(Tab);
+            Timer.Stop();
+            Sorting.PrintIndexAndValues(Tab);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tri/Sorting.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tri
+{
+    class Sorting
+    {
+        public static int[] FillTab()
+        {
+            const int SIZE = 10;
+
+            int[] Tab = new int[SIZE];
+            Random rnd = new Random();
+
+            for (int i = 0; i < Tab.Length; i++)
+            {
+                int num = rnd.Next(-10, SIZE);
+                Tab[i] = num;
+            }
+            return Tab;
+        }
+
+        public static void Reverse(ref int a, ref int b)
+        {
+            int tmp;
+
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        public static void SortingTab(int[] Tab)
+        {
+            bool b = false;
+            int i = 0;
+
+            while (b == false && i <= Tab.Length)
+            {
+                b = true;
+                for (int y = 0; y < Tab.Length - i - 1; y++)
+                {
+                    if (Tab[y] > Tab[y + 1])
+                    {
+                        Reverse(ref Tab[y], ref Tab[y + 1]);
+                        b = false;
+                    }
+                }
+            }
+        }
+
+        public static void PrintIndexAndValues(int[] Tab)
+        {
+            for (int i = Tab.GetLowerBound(0); i <= Tab.GetUpperBound(0); i++)
+            {
+
+                Console.WriteLine("\t[{0}]:\t{1}", i, Tab.GetValue(i));
+            }
+
+            /*for(int i = 0; i < Tab.Length; i++)
+            {
+                Console.Write("{0}, ", Tab[i]);
+            }*/
+        }
+    }
+}
