a86a11293411cd251c74a3f09f3f4f42f444fccb
DIDIx13 <DIDI.ameli@gmail.com>
2018-01-19 14:28:49 +0100

    Add files via upload

---
+++ Auptic/Auptic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auptic
+{
+    class Program
+    {
+        public static void Methode(int[] t)
+        {
+            t[0] = t[0] + 24;
+        }
+        static void Main(string[] args)
+        {
+            int[] tabTest = new int[] { 12 };
+
+            Methode(tabTest);
+            Console.WriteLine("tabTest[0] à la fin: {0}", tabTest[0]);
+            Console.ReadKey();
+        }
+    }
+}
