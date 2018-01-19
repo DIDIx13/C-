6e8a66d16dce5eef794fb3f7561b0fe4c61d19e5
DIDIx13 <DIDI.ameli@gmail.com>
2018-01-19 14:26:43 +0100

    Add files via upload

---
+++ Personne/Personne/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne p1 = new Personne();
+            p1.setName("Ameli");
+            p1.setFirstname("Darwin");
+            p1.setDateNaissance("06.01.2000");
+            Console.WriteLine(p1.getAge());
+            Console.ReadKey();
+        }
+    }
+    public class Personne
+    {
+        private string strName;
+        private string strFirstname;
+        private DateTime _dateNaissance;
+
+        public void setDateNaissance(string date)
+        {
+            this._dateNaissance = DateTime.Parse(date, System.Globalization.CultureInfo.CurrentCulture);
+
+        }
+        public void setName(string Name)
+        {
+            this.strName = Name;
+        }
+        public void setFirstname(string Firstname)
+        {
+            this.strFirstname = Firstname;
+        }
+        public int getAge()
+        {
+            DateTime today = DateTime.Today; // Save today's date..
+            
+            int age = today.Year - _dateNaissance.Year; // Calculate the age.
+
+            return age; 
+        }
+    }
+}
