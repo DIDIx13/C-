c050fad1b8a8d424f4acf8dd4bbe30981703fe0b
DIDIx13 <DIDI.ameli@gmail.com>
2018-01-19 14:24:53 +0100

    Add files via upload

---
+++ Fourmis/Fourmis/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fourmis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Livre unLivre = new Livre();
+
+        }
+        public class Livre
+        {
+            private string strTitre;
+            private int INbPage;
+            private string strAuteur;
+            private DateTime _dateParution;
+            private string strIsbn;
+            private double dblPrix;
+            
+            public void setTitre(string Titre)
+            {
+                this.strTitre = Titre;
+            }
+            public void setAuteur(string Auteur)
+            {
+                this.strAuteur = Auteur;
+            }
+            public void setIsbn(string Isbn)
+            {
+                this.strIsbn = Isbn;
+            }
+            public void setINbPage(int INbPage)
+            {
+                this.INbPage = INbPage;
+            }
+            public void set(string Titre)
+            {
+                this.strTitre = Titre;
+            }
+        }
+    }
+}
