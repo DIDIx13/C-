cccbeebc6ba93a6c568be890768b6a322be15735
DIDIx13 <DIDI.ameli@gmail.com>
2018-02-19 16:24:39 +0100

    Add TestType

---
+++ TestType/TestList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write(33);
+            Write(12.5);
+            Console.ReadKey(true);
+        }
+
+        static void Write<T>(T val)
+        {
+            Console.WriteLine(val.ToString() + " est de type : " + typeof(T));
+        }
+    }
+}
