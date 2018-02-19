940d368278de75de0929bbc7dff862bb88e55ac5
DIDIx13 <DIDI.ameli@gmail.com>
2018-02-19 16:28:00 +0100

    Add files via upload

---
+++ Garniture.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaTDD
+{
+    public class Garniture
+    {
+        private int iQuantite;
+        private double dblPrixDeRevient;
+
+        public Garniture (string nom, double prixDeRevient, int quantite)
+        {
+            Nom = nom;
+            Update(prixDeRevient);
+            Update(quantite);
+        }
+        public string Nom { get; private set; }
+
+        public double getPrixDeRevient()
+        {
+            return dblPrixDeRevient;
+        }
+        public int getQuantite()
+        {
+            return iQuantite;
+        }
+
+        public int Quantite
+        {
+            get { return iQuantite; }
+        }
+        public void Update(double prixDeRevient)
+        {
+            dblPrixDeRevient = prixDeRevient;
+            if (prixDeRevient<0)
+            {
+                dblPrixDeRevient = 0.0;
+            }
+        }
+        public void Update(int quantite)
+        {
+            iQuantite = quantite;
+            if (quantite < 5 || quantite > 70)
+            {
+                iQuantite = 15;
+            }
+        }
+        public void Update(double prixDeRevient, int quantite)
+        {
+            Update(prixDeRevient);
+            Update(quantite);
+        }
+
+        public override string ToString()
+        {
+            return Nom + ", " + iQuantite.ToString()
+                + "g (" + dblPrixDeRevient.ToString("F2") + " CHF)";
+        }
+    }
+}
---
+++ Pizza.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaTDD
+{    
+    class Pizza
+    {
+        private const int MAX_GARNITURES = 8;
+
+        private int iNumero;
+        private string strNom;
+        private double dblPrixDeVente;
+        private List<Garniture> _garnitures;
+
+        public Pizza(int num, string nom)
+        {
+          
+        }
+        public Pizza(int num, string nom, double prixDeVente)
+        {
+          
+        }
+
+        private void initialisation(int num, string nom, double prixDeVente)
+        {
+          
+        }
+
+        public string LireGarnitures()
+        {
+            string strGarnitures = "";
+            return strGarnitures;
+        }
+
+        public bool AjouterGaniture(string nom, double prixRevient, int quantite)
+        {
+            bool bReussi = false;
+            return bReussi;
+        }
+    }
+}
