b60742985bc5e4fc3b74c8a8b0ce99218e5a5d11
Darwin Ameli <DIDI.ameli@gmail.com>
2020-06-09 19:03:25 +0200

    Add files via upload

---
+++ Rnd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime;
+
+namespace Rnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+            Console.Write("{0}", random);
+            Console.ReadKey();
+
+        }
+    }
+}
